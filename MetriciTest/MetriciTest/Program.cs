e512ee4e28652b9e2da8f3734148d86ccc752734
Roman <roman.lazygeek@gmail.com>
2016-10-22 15:07:07 +0300

    находим побочную диагональ

--- MetriciTest/MetriciTest/Program.cs
+++ MetriciTest/MetriciTest/Program.cs
@@ -14,21 +14,36 @@ namespace MetriciTest
 
             Random randomize = new Random();
 
+           
+
             //Console.WriteLine("Enter elements of array: ");
             for (int i = 0; i < matrix.GetLength(1); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Cyan;
                     matrix[i, j] = randomize.Next(1, 100);
                     Console.Write("{0}\t", matrix[i, j]);
                     //Console.Write("Enter value of [{0}, {1}]:", i, j);
                     //matrix[i, j] = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("{0}\t", matrix[i, j]);
                 }
                 Console.WriteLine();
             }
 
+            var min = matrix[0,matrix.GetLength(1)-1];
+            int k = 0;
+               for (int j = matrix.GetLength(1)-1; j >= 0; j--)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    if (min > matrix[k,j])
+                    {
+                        min = matrix[k,j];
+                    }
+                    k++;
+                }
+                Console.WriteLine();
+
+            Console.WriteLine(min);
             Console.WriteLine("\n");
 
             Console.ReadLine();
