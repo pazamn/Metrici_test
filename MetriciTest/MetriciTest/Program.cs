d082cbfc7bbac57149b0dcef1919e61c1bd470b6
Roman <roman.lazygeek@gmail.com>
2016-10-22 10:20:37 +0300

    restored random array

--- MetriciTest/MetriciTest/Program.cs
+++ MetriciTest/MetriciTest/Program.cs
@@ -14,12 +14,12 @@ namespace MetriciTest
 
             Random randomize = new Random();
 
-            Console.WriteLine("Enter elements of array: ");
+            //Console.WriteLine("Enter elements of array: ");
             for (int i = 0; i < matrix.GetLength(1); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
-                    //Console.ForegroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Red;
                     matrix[i, j] = randomize.Next(1, 100);
                     Console.Write("{0}\t", matrix[i, j]);
                     //Console.Write("Enter value of [{0}, {1}]:", i, j);
